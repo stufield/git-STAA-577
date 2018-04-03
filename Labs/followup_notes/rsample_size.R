2b0ddf6ef82184689e25c751cbca44c926028a90
Stu Field <sfield@somalogic.com>
2018-04-02 22:36:45 -0600

    Update rsample reprex to use pryr::object_size() not object.size()

--- Labs/followup_notes/rsample_size.R
+++ Labs/followup_notes/rsample_size.R
@@ -2,16 +2,16 @@
 reprex::reprex({
   suppressMessages(library(tibble))
   suppressMessages(library(magrittr))
+  suppressMessages(library(pryr))
   suppressMessages(library(rsample))
-  iris %>%
-    as.tibble %T>% {
-    object.size(.) %>%
-        format(units = "KB") %>%
-        stringr::str_c("Original Size: ", .) %>%
-        print()
-    } %>%
-    rsample::bootstraps(times = 1000) %>%
-    object.size(.) %>%
-    format(units = "KB") %>%
-    stringr::str_c("Bootstrap Size: ", .)
-})
+  iris %<>% as.tibble
+  x1 <- pryr::compare_size(iris)
+  x2 <- rsample::bootstraps(iris, times = 1000) %>%
+    pryr::compare_size(.)
+  # Original Size:
+  print(x1)
+  # Resample Size:
+  print(x2)
+  # Ratio:
+  print(x2/x1)
+}, outfile = "blah")
