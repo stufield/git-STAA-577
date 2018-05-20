f91179ed23de4231a8e31dbdf0b61c7d310bb52a
Stu Field <sfield@somalogic.com>
2018-05-20 13:26:50 -0600

    update rsample size reprex to use the Hadley's `lobstr` package rather than `pryr` for object sizes

--- Labs/followup_notes/rsample_size.R
+++ Labs/followup_notes/rsample_size.R
@@ -2,16 +2,21 @@
 reprex::reprex({
   suppressMessages(library(tibble))
   suppressMessages(library(magrittr))
-  suppressMessages(library(pryr))
   suppressMessages(library(rsample))
-  iris %<>% as.tibble
-  x1 <- pryr::compare_size(iris)
+  # devtools::install_github("r-lib/lobstr")  # install dev version
+  suppressMessages(library(lobstr))
+  compare_size <- function(x) {
+    c(base   = utils::object.size(x),
+      lobstr = lobstr::obj_size(x))
+  }
+  iris %<>% as.tibble()
+  x1 <- compare_size(iris)
   x2 <- rsample::bootstraps(iris, times = 1000) %>%
-    pryr::compare_size(.)
+    compare_size()
   # Original Size:
   print(x1)
   # Resample Size:
   print(x2)
   # Ratio:
   print(x2/x1)
-}, outfile = "blah")
+}, outfile = "rsample_size")
