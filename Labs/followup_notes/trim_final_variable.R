233153b2bfc7a71bf11a04c383c51f23c52ab429
Stu <stu.g.field@gmail.com>
2018-11-14 11:45:06 -0700

    update data_wrangling lab; update variable trimming with last_col() from tidyselect

--- Labs/followup_notes/trim_final_variable.R
+++ Labs/followup_notes/trim_final_variable.R
@@ -1,19 +1,33 @@
+#' Trim final variable in a data frame; there are many ways to skin this cat
+#' -----------------------
 
-library(ISLR)
-dim(Smarket)
-names(Smarket)
+options(tidyverse.quiet = TRUE)
+library(tidyverse)
+dim(mtcars)
+names(mtcars)
 
-# Using dplyr in tidyverse
-new <- dplyr::select(Smarket, -Direction)
+
+#' If you know the variable name
+#' -------------------
+#' Use `dplyr::select()`
+new <- mtcars %>% select(-carb)
 dim(new)
 names(new)
 
-# Using base R; final column
-new2 <- Smarket[, -ncol(Smarket)]
+#' Or using `base R + which()`
+new2 <- mtcars[, -which(names(mtcars) == "carb")]
 dim(new2)
 names(new2)
 
-# Using base R; using which()
-new3 <- Smarket[, -which(names(Smarket)=="Direction")]
+
+#' If you do NOT know the variable name
+#' -------------------
+#' Use `dplyr::select()` with helper `tidyselect::last_col()`
+new3 <- mtcars %>% select(-tidyselect::last_col())
 dim(new3)
 names(new3)
+
+#' Or using `base R + ncol()`
+new4 <- mtcars[, -ncol(mtcars)]
+dim(new4)
+names(new4)
